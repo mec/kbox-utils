06aae59b72300cad688e4d59ec657505b8ea3fa9
Mec Rawlings <mec.rawlings@gmail.com>
2017-06-23 11:24:36 +0100

    Add some error handling.

---
+++ bin/kbox-utils.js
@@ -0,0 +1,147 @@
+#! /usr/bin/env node
+
+'use strict'
+const exec = require('child_process').exec
+const fs = require('fs');
+
+let connection
+
+// get the argument
+let userArgs = process.argv.slice(2)
+let action = userArgs[0]
+
+exec('kbox services', {
+  cwd: process.cwd()
+}, function(error, stdout, stderr) {
+  if (error) {
+    console.error('Something went wrong with kbox, are you in a Kalabox app environment?')
+    process.exit(1)
+  } else {
+    // create array from JSON
+    let services = JSON.parse(stdout)
+    if (services) {
+      // create some variables to hodling our stuff
+      let webAddress
+      let dbConnection
+      // loop over the servies array and locate the db and web items
+      services.forEach(function(item, index, services) {
+        if ( item.name === 'db') {  dbConnection = services[index] }
+        if	( item.name === 'web') {  webAddress = services[index] }
+      })
+      // create object with our data
+      connection = {
+        'web' : {
+          'secure' : webAddress.url[1],
+          'insecure' : webAddress.url[0],
+        },
+        'db' : dbConnection.external_connection_info
+      }
+      switch(action) {
+        case 'opens':
+          openInBrowser(connection, true);
+          break
+        case 'open':
+          openInBrowser(connection, false);
+          break
+        case 'connectdb':
+          createSequelProDoc(connection);
+          break
+        default:
+          console.error("Sorry, I don't know how to '" + action + "''")
+      }
+    } else {
+      console.error('Could not read kbox services, are you in a Kalabox app environment?')
+      process.exit(1)
+    }
+  }
+})
+
+function openInBrowser(connection, secure) {
+  if (!connection.web) {
+    console.error('Could not find web settings, check kbox services?')
+    process.exit(1)
+  }
+  if (secure) {
+      exec('open ' + connection.web.secure)
+  } else {
+      exec('open ' + connection.web.insecure)
+  }
+}
+
+function createSequelProDoc(connection) {
+  if (connection.db) {
+    const sequelProDoc = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>ContentFilters</key>
+      <dict/>
+      <key>auto_connect</key>
+      <true/>
+      <key>data</key>
+      <dict>
+          <key>connection</key>
+          <dict>
+              <key>database</key>
+              <string>${connection.db.database}</string>
+              <key>host</key>
+              <string>${connection.db.host}</string>
+              <key>password</key>
+              <string>${connection.db.password}</string>
+              <key>port</key>
+              <integer>${connection.db.port}</integer>
+              <key>rdbms_type</key>
+              <string>mysql</string>
+              <key>sslCACertFileLocation</key>
+              <string></string>
+              <key>sslCACertFileLocationEnabled</key>
+              <integer>0</integer>
+              <key>sslCertificateFileLocation</key>
+              <string></string>
+              <key>sslCertificateFileLocationEnabled</key>
+              <integer>0</integer>
+              <key>sslKeyFileLocation</key>
+              <string></string>
+              <key>sslKeyFileLocationEnabled</key>
+              <integer>0</integer>
+              <key>type</key>
+              <string>SPTCPIPConnection</string>
+              <key>useSSL</key>
+              <integer>0</integer>
+              <key>user</key>
+              <string>${connection.db.user}</string>
+          </dict>
+      </dict>
+      <key>encrypted</key>
+      <false/>
+      <key>format</key>
+      <string>connection</string>
+      <key>queryFavorites</key>
+      <array/>
+      <key>queryHistory</key>
+      <array/>
+      <key>rdbms_type</key>
+      <string>mysql</string>
+      <key>rdbms_version</key>
+      <string>5.5.44</string>
+      <key>version</key>
+      <integer>1</integer>
+    </dict>
+    </plist>
+    `
+    fs.writeFile('/tmp/connect.spf', sequelProDoc , function (err) {
+      if (err) return console.error(err)
+      else {
+        exec('open /tmp/connect.spf', function() {
+          //delete the tmp file now
+          fs.unlink('/tmp/connect.spf')
+        })
+      }
+
+    })
+  } else {
+    console.error('Could not find database settings, check kbox services?')
+    process.exit(1)
+  }
+}
