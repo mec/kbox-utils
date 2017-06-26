6204a1c1d4e56c3235bddea16bd776f08dca3bcd
Mec Rawlings <mec.rawlings@gmail.com>
2017-06-26 21:36:12 +0100

    Add more error handling.

--- bin/kboxutils.js
+++ bin/kboxutils.js
@@ -2,13 +2,16 @@
 
 'use strict'
 const exec = require('child_process').exec
-const fs = require('fs');
-
-let connection
+const fs = require('fs')
 
 // get the argument
 let userArgs = process.argv.slice(2)
 let action = userArgs[0]
+
+if (!action) {
+  console.error('kbox-util requires either open, opens or connectdb argument.')
+  process.exit(1)
+}
 
 exec('kbox services', {
   cwd: process.cwd()
@@ -18,58 +21,71 @@ exec('kbox services', {
     process.exit(1)
   } else {
     // create array from JSON
-    let services = JSON.parse(stdout)
-    if (services) {
-      // create some variables to hodling our stuff
-      let webAddress
-      let dbConnection
-      // loop over the servies array and locate the db and web items
-      services.forEach(function(item, index, services) {
-        if ( item.name === 'db') {  dbConnection = services[index] }
-        if	( item.name === 'web') {  webAddress = services[index] }
-      })
-      // create object with our data
-      connection = {
-        'web' : {
-          'secure' : webAddress.url[1],
-          'insecure' : webAddress.url[0],
-        },
-        'db' : dbConnection.external_connection_info
-      }
-      switch(action) {
-        case 'opens':
-          openInBrowser(connection, true);
-          break
-        case 'open':
-          openInBrowser(connection, false);
-          break
-        case 'connectdb':
-          createSequelProDoc(connection);
-          break
-        default:
-          console.error("Sorry, I don't know how to '" + action + "''")
-      }
-    } else {
-      console.error('Could not read kbox services, are you in a Kalabox app environment?')
-      process.exit(1)
+    try {
+      let services = JSON.parse(stdout)
+      parseServices(services)
+    } catch (e) {
+      console.log('json error', e)
     }
   }
 })
+
+function parseServices(services) {
+  if (services !== 'App is not running.') {
+    // create some variables to holding our stuff
+    let connection
+    let webAddress
+    let dbConnection
+    // loop over the servies array and locate the db and web items
+    services.forEach(function(item, index, services) {
+      if ( item.name === 'db') {  dbConnection = services[index] }
+      if ( item.name === 'web') {  webAddress = services[index] }
+    })
+    // create object with our data
+    connection = {
+      'web' : {
+        'secure' : webAddress.url[1],
+        'insecure' : webAddress.url[0],
+      },
+      'db' : dbConnection.external_connection_info
+    }
+    switch(action) {
+      case 'opens':
+        openInBrowser(connection, true);
+        break
+      case 'open':
+        openInBrowser(connection, false);
+        break
+      case 'connectdb':
+        createSequelProDoc(connection);
+        break
+      default:
+        console.error("Sorry, I don't know how to '" + action + "''")
+    }
+  } else {
+    console.error('Could not read kbox services, are you in a running Kalabox app environment?')
+    process.exit(1)
+  }
+}
 
 function openInBrowser(connection, secure) {
   if (!connection.web) {
     console.error('Could not find web settings, check kbox services?')
     process.exit(1)
-  }
-  if (secure) {
-      exec('open ' + connection.web.secure)
   } else {
-      exec('open ' + connection.web.insecure)
+    if (secure) {
+        exec('open ' + connection.web.secure)
+    } else {
+        exec('open ' + connection.web.insecure)
+    }
   }
 }
 
 function createSequelProDoc(connection) {
-  if (connection.db) {
+  if (!connection.db) {
+    console.error('Could not find database settings, check kbox services?')
+    process.exit(1)
+  } else {
     const sequelProDoc = `
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -133,14 +149,8 @@ function createSequelProDoc(connection) {
     fs.writeFile('/tmp/connect.spf', sequelProDoc , function (err) {
       if (err) return console.error(err)
       else {
-        exec('open /tmp/connect.spf', function() {
-          //delete the tmp file now
-        })
+        exec('open /tmp/connect.spf')
       }
-
     })
-  } else {
-    console.error('Could not find database settings, check kbox services?')
-    process.exit(1)
   }
 }
